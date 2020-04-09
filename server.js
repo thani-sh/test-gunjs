ced45e28163d79e83f4d5122e8f1ac237f95568b
Muhammed Thanish <mnmtanish@gmail.com>
2020-04-09 12:45:02 +0530

    Add endpoint to serve TURN urls

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const gun = require('gun');
+
+const XIRSYS_URL = process.env.XIRSYS_URL;
 
 const app = express();
 
 app.use(gun.serve);
 app.use(express.static('public'));
 
-const server = app.listen(process.env.PORT);
-const gunServer = gun({ file: 'data', web: server });
+app.get('/turn', async function(req, res) {
+    const content = JSON.stringify({ format: 'urls' });
+    const headers = { 'Content-Type': 'application/json' };
+    const _res = await fetch(XIRSYS_URL, { method: 'PUT', body: content, headers });
+    const data = await _res.json();
+    res.json(data.v);
+});
+
+const server = app.listen(process.env.PORT || 3000);
+const {} = gun({ file: 'data', web: server });
