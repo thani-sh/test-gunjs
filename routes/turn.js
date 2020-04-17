11b93fc978260b379115b906969c5dc976f8f2d2
Muhammed Thanish <mnmtanish@gmail.com>
2020-04-17 13:41:25 +0530

    Reduce Xirsys calls

--- routes/turn.js
+++ routes/turn.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const fetch = require('node-fetch');
 
+// NOTE: time is in seconds
+const TURN_EXPIRE = 60 * 5;
+
 async function getDefaultIce() {
     return {
         iceServers: { urls: 'stun:stun.l.google.com:19302' },
     };
 }
 
+let cachedIce = { time: 0, data: null };
 async function getXirsysIce() {
-    const content = JSON.stringify({ format: 'urls' });
+    if ( Date.now() < cachedIce.time + TURN_EXPIRE * 1000 ) {
+        return cachedIce.data;
+    }
+    const content = JSON.stringify({
+        format: 'urls',
+        expire: Math.floor(TURN_EXPIRE * 0.8),
+    });
     const headers = { 'Content-Type': 'application/json' };
     const options = { method: 'PUT', body: content, headers };
     const data = await fetch(config.XIRSYS_URL, options)
         .then(res => res.json());
-    return data.v;
+    cachedIce = { time: Date.now(), data: data.v };
+    return cachedIce.data;
 }
 
 /**
