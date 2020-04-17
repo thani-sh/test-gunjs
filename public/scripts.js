93c3f60887db3c4106d3791b6797a2e1abcb088c
Muhammed Thanish <mnmtanish@gmail.com>
2020-04-17 09:39:12 +0530

    Optimize the webapp
     - cache username
     - use css transform
     - remove unused code

--- public/scripts.js
+++ public/scripts.js
@@ -3,17 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const elFormUser = document.getElementById('form-user');
     const elFormName = document.getElementById('form-user-name');
     const elPageGame = document.getElementById('page-game');
+    const storedUser = localStorage.getItem('user');
+
+    if (storedUser) {
+        elPageGame.style.display = 'flex';
+        loadGame(storedUser);
+    } else {
+        elPageUser.style.display = 'flex';
+    }
 
     elFormUser.addEventListener('submit', e => {
+        const user = elFormName.value;
         e.preventDefault();
-        gotoGame();
-        loadGame();
+        localStorage.setItem('user', user);
+        elPageGame.style.display = 'flex';
+        loadGame(user);
     });
-
-    function gotoGame() {
-        elPageUser.style.display = 'none';
-        elPageGame.style.display = 'block';
-    }
 
     async function getICEServers() {
         const turnServers = await fetch(`${location.origin}/turn`)
@@ -25,10 +30,9 @@ document.addEventListener('DOMContentLoaded', () => {
         ]
     }
 
-    async function loadGame() {
-        const user = elFormName.value;
+    async function loadGame(user) {
         const data = {
-            [user]: { x: 0, y: 0, el: createPoint(user) },
+            [user]: { x: -50, y: -50, el: createPoint(user) },
         };
 
         const root = Gun({
@@ -36,30 +40,18 @@ document.addEventListener('DOMContentLoaded', () => {
             rtc: { iceServers: await getICEServers() },
         });
 
-        let sendPosition = () => {};
-
-        if (localStorage.getItem('dam')) {
-            const dam = root.back('opt.mesh');
-            dam.hear.GameData = (msg, peer) => {
-                const { name, x, y } = msg;
+        root.on('in', function (msg) {
+            if (msg.cgx) {
+                const { name, x, y } = msg.cgx;
                 updateData(name, x, y);
-            };
-            sendPosition = (x, y) => {
-                dam.say({ dam: 'GameData', name: user, x, y });
-            };
-        } else {
-            root.on('in', function (msg) {
-                if (msg.cgx) {
-                    const { name, x, y } = msg.cgx;
-                    updateData(name, x, y);
-                }
-                this.to.next(msg);
-            });
-            sendPosition = (x, y) => {
-                const id = Math.random().toString().slice(2);
-                root.on( 'out', { '#': id, cgx: { name: user, x, y }});
-            };
-        }
+            }
+            this.to.next(msg);
+        });
+
+        function sendPosition(x, y) {
+            const id = Math.random().toString().slice(2);
+            root.on( 'out', { '#': id, cgx: { name: user, x, y }});
+        };
 
         function updateData(name, x, y) {
             if (!data[name]) {
@@ -84,8 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
         function render() {
             for (const name of Object.keys(data)) {
                 const { el, x, y } = data[name];
-                el.style.left = `${x}px`;
-                el.style.top = `${y}px`;
+                el.style.transform = `translate(${x}px, ${y}px)`;
             }
         }
         function schedule() {
